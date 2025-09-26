0b7b0380ea9bca12d03d0b311ce4548a7b5f85a5
Will Evonosky <will@orbitalsidekick.com>
2025-09-26 21:09:23 +0000

    Swapped out docopt for docopt-ng, removed use of distutils

--- setup.py
+++ setup.py
@@ -3,20 +3,25 @@ from setuptools import setup
 # Version moved to pynitf/version.py so we have one place it is defined.
 exec(open("pynitf/version.py").read())
 
-setup(name='pynitf',
-      version=__version__,
-      description='This is a package for reading and writing NITF',
-      url='https://github.jpl.nasa.gov/Cartography/pynitf',
-      author='Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>, Philip Yoon <Philip.J.Yoon@jpl.nasa.gov>',
-      author_email='Mike.M.Smyth@jpl.nasa.gov',
-      license='Copyright 2020, California Institute of Technology. ALL RIGHTS RESERVED. U.S. Government Sponsorship acknowledged.',
-      packages=['pynitf'],
-      scripts=["bin/nitf_diff", "bin/nitf_info", "bin/nitf_json_delta",
-               "bin/explore_nitf"],
-      install_requires=[
-          'numpy',
-          'docopt'
-      ],
-      setup_requires=["pytest-runner",],
-      tests_requires=["pytest","h5py"],
-      zip_safe=False)
+setup(
+    name="pynitf",
+    version=__version__,
+    description="This is a package for reading and writing NITF",
+    url="https://github.jpl.nasa.gov/Cartography/pynitf",
+    author="Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>, Philip Yoon <Philip.J.Yoon@jpl.nasa.gov>",
+    author_email="Mike.M.Smyth@jpl.nasa.gov",
+    license="Copyright 2020, California Institute of Technology. ALL RIGHTS RESERVED. U.S. Government Sponsorship acknowledged.",
+    packages=["pynitf"],
+    scripts=[
+        "bin/nitf_diff",
+        "bin/nitf_info",
+        "bin/nitf_json_delta",
+        "bin/explore_nitf",
+    ],
+    install_requires=["numpy", "docopt-ng"],
+    setup_requires=[
+        "pytest-runner",
+    ],
+    tests_requires=["pytest", "h5py"],
+    zip_safe=False,
+)
